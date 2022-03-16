e5dfcf2442500f2d7cf88990b0b86b6bbe03ded9
Victor Hugo De la O Martínez <hugo_4295@hotmail.com>
2022-03-15 19:35:02 -0600

    subiendo los archivos faltantes

---
+++ src/Operaciones/Basicas.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Operaciones;
+
+/**
+ *
+ * @author hugo4295
+ */
+public class Basicas {
+    
+    public int Sumar(int n1, int n2){
+        return (n1+n2);
+    }
+    
+}
