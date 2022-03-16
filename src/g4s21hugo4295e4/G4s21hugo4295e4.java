e3c2241cfa1a5b6e4e7d43dbe72e662da81147ac
Victor Hugo De la O Martínez <hugo_4295@hotmail.com>
2022-03-15 19:29:16 -0600

    cuarto programa de la clase del 4s21

---
+++ src/g4s21hugo4295e4/G4s21hugo4295e4.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package g4s21hugo4295e4;
+
+import Formularios.FrmPrincipal;
+
+/**
+ *
+ * @author hugo4295
+ */
+public class G4s21hugo4295e4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new FrmPrincipal().setVisible(true);
+    }
+    
+}
